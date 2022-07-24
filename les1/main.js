c9db7c2d16b1a3774535e84cf06ef6eec86447a2
MykolaRak <natonrak@gmail.com>
2022-07-24 23:06:25 +0300

    done

---
+++ les1/main.js
@@ -0,0 +1,84 @@
+// 1.- Створити змінні.
+//     Присвоїти кожному з них значення:
+//     'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+
+let a = 'hello';
+let b = 'owu';
+let c = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let l = 3.14;
+let m = 2.7;
+let n = 16;
+let o = true;
+let p = false;
+
+// 2.Вивести кожну змінну за допомогою: console.log , alert, document.write
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(l);
+console.log(m);
+console.log(n);
+console.log(o);
+console.log(p);
+
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(e);
+// alert(f);
+// alert(g);
+// alert(h);
+// alert(l);
+// alert(m);
+// alert(n);
+// alert(o);
+// alert(p);
+
+document.write(a);
+document.write(b);
+document.write(c);
+document.write(d);
+document.write(e);
+document.write(f);
+document.write(g);
+document.write(h);
+document.write(l);
+document.write(m);
+document.write(n);
+document.write(o);
+document.write(p);
+
+// 3. Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person ' +
+// '(Не об'єкт, просто за допомоги конкатенації)
+let firstName = "Mykola";
+let middleName = "Stepanovych";
+let lastName = "Rak";
+let person = firstName+' '+middleName+' '+lastName;
+console.log(person)
+
+
+// 4 За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+let num = 100;
+let str = '100';
+let bool = true;
+console.log(typeof num,typeof str,typeof bool);
+
+
+
+// 4. За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+// let name = prompt('name?');
+// let lastname2 = prompt('Lastname?');
+// let age = prompt('Age?');
+// console.log(name,lastname2,age);
